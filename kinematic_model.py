e660e91200b0e6b04cb3152c61ba77bd4c8511cb
MengXiangzhe <45839654+mmxa@users.noreply.github.com>
2019-07-03 10:36:55 +0800

    Add files via upload

---
+++ kinematic_model.py
@@ -0,0 +1,39 @@
+import math
+import matplotlib.pyplot as plt
+#reference: https://blog.csdn.net/AdamShan/article/details/78696874
+
+class Kinematic_model:
+    def __init__(self, x=0, y=0, psi=0, v=1, f_len=1.3, r_len=1.5):
+        self.x = x
+        self.y = y
+        self.psi = psi
+        self.v = v
+        self.f_len = f_len
+        self.r_len = r_len
+
+    def get_state(self):
+        return self.x, self.y, self.psi, self.v
+
+    def update_state(self, a, delta, dt):
+        beta = math.atan((self.r_len / (self.r_len + self.f_len)) * math.tan(delta))
+        self.x = self.x + self.v * math.cos(self.psi + beta) * dt
+        self.y = self.y + self.v * math.sin(self.psi + beta) * dt
+        self.psi = self.psi + self.v / self.r_len * math.sin(beta) * dt
+        self.v = self.v + a * dt
+
+car = Kinematic_model(x=5, y=5, psi=0, v=5, f_len=1.3, r_len=1.5)
+step = 1000                   #define a step for simulate
+dt = 0.01
+his_x = []                    #recording a track
+his_y = []
+plt.axis([-10, 50, -10, 50])
+print ("the simulate time is :  ", dt*step)
+
+while (step > 0):         #update model and restore the state
+    his_x.append(car.x)
+    his_y.append(car.y)
+    car.update_state(0.02, 0.1+step*0.001, dt)
+    step = step - 1
+    plt.plot(his_x, his_y, "-k")
+    plt.pause(0.01)
+plt.show()
